2d848c3d14595f759fee8f19d7ab55aed7c768c8
Shane Zatezalo <shane@lottadot.com>
2012-10-09 00:09:17 -0400

    Add (possible future) beginnings of a table cell for the posts tableview.

---
+++ AppNetDemo/AppNetDemo/Classes/ANDPostTableCell.h
@@ -0,0 +1,13 @@
+//
+//  ANDPostTableCell.h
+//  AppNetDemo
+//
+//  Created by Shane Zatezalo on 10/8/12.
+//  Copyright (c) 2012 Lottadot LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ANDPostTableCell : NSObject
+
+@end
