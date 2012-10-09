2277c70db769d9a7f9ba85e88ede6e5f11424252
Shane Zatezalo <shane@lottadot.com>
2012-10-09 00:06:47 -0400

    Add AppDotNet Constants for Keys

--- AppNetDemo/AppNetDemo/Classes/ConstantList.h
+++ AppNetDemo/AppNetDemo/Classes/ConstantList.h
@@ -9,3 +9,17 @@
 #include "Constants.h"
 
 STR_CONST(kTESTFLIGHTTEAMTOKEN, "blahblahblah");
+STR_CONST(kAppDotNetClientID, "meh");
+STR_CONST(kAppDotNetClientSecret, "meh");
+STR_CONST(kAppDotNetAuthorizeURL, "meh");
+STR_CONST(kAppDotNetAccessTokenURL, "meh");
+STR_CONST(kAppDotNetCallbackURL, "meh");
+STR_CONST(kAppDotNetDemoUserToken, "meh");
+
+// From https://alpha.app.net/developer/apps/
+//Client ID	...
+//Client Secret	...
+//Authorize URL	https://alpha.app.net/oauth/authenticate
+//Access Token URL	https://alpha.app.net/oauth/access_token
+//Callback URL
+//http://tapplyapp.com/auth
