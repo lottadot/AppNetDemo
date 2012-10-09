b4e3ad8f9a9944b6c753f7283a1d5816b7021653
Shane Zatezalo <shane@lottadot.com>
2012-10-09 00:04:01 -0400

    Add second controller wiring

--- AppNetDemo/AppNetDemo/ANDSecondViewController.h
+++ AppNetDemo/AppNetDemo/ANDSecondViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ANDSecondViewController : UIViewController
+@interface ANDSecondViewController : UIViewController <UITableViewDelegate>
 
 @end
--- AppNetDemo/AppNetDemo/ANDSecondViewController.m
+++ AppNetDemo/AppNetDemo/ANDSecondViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ANDSecondViewController.h"
+#import "AppNetKit.h"
 
 @interface ANDSecondViewController ()
-
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *tableData;
 @end
 
 @implementation ANDSecondViewController
@@ -27,13 +29,65 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[[ANSession defaultSession] setAccessToken:kAppDotNetDemoUserToken];
+    
+    [[ANSession defaultSession] postsInStreamWithCompletion:^(ANResponse *response,
+                                                              NSArray *posts, NSError *error) {
+        if(!posts) {
+            UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:NSLocalizedString(@"Could not load posts", nil)
+                                                        message:error.localizedDescription
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:NSLocalizedString(@"OK", nil), nil];
+            [alert show];
+            return;
+        } else {
+            NSLog(@"Posts:%@", posts);
+            _tableData = posts;
+            [_tableView reloadData];
+        }
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView Delegate Protocol
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)table {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section {
+    NSInteger numberOfRows = 0;
+    if (_tableData) {
+        numberOfRows = [_tableData count];
+    }
+    return numberOfRows;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+		 cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                       reuseIdentifier:CellIdentifier];
+	}
+    
+    ANPost *post = (ANPost *)[_tableData objectAtIndex:indexPath.row];
+    ANUser *poster = [post user];
+    cell.textLabel.text = [poster username];
+    cell.detailTextLabel.text = [post text];
+    return cell;
 }
 
 @end
